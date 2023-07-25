30ccb4338c9b7f35d94cc14a890ab0ca40b3a8a1
CaioKN7 <galdinocaiost@gmail.com>
2023-07-25 20:24:46 -0300

    Exercício 2 refeito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,19 +1,33 @@
 //Exercício 2
 
-function imprimePosicao() {
 const apenasNumeros = [4, 1, 90, 200, 74, 7, 43]
 const apenasStrings = ["Zico", "Adílio", "Nunes", "Leandro", "Júnior"]
 const todosOsTipos = ["Adriano", "Petkovic", 10, 43, true, false]
 
-console.log(apenasNumeros[0])
-console.log(apenasStrings[1])
-console.log(todosOsTipos[2])
+
+
+function imprimeQuantidadeDeItens(numeros, strings, tipos) {
+
+console.log(numeros.length)
+console.log(strings.length)
+console.log(tipos.length)
+
+}
+
+imprimeQuantidadeDeItens(apenasNumeros, apenasStrings, todosOsTipos)
+
+
+function imprimePosicao(numeros, strings, tipos) {
+
+console.log(numeros[0])
+console.log(strings[1])
+console.log(tipos[2])
 const verificaNumeroApenasNumeros = apenasNumeros.includes(90);
 const verificaStringEmTodosOsTipos = todosOsTipos.includes("Zico");
 
 console.log(verificaNumeroApenasNumeros);
 console.log(verificaStringEmTodosOsTipos);
-
+    
 }
 
-imprimePosicao()
+imprimePosicao(apenasNumeros, apenasStrings, todosOsTipos)
